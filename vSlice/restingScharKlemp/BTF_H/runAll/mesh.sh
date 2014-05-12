23cc6e449bc1b411afc0e3b846b82c762c3cfc3d
James Shaw <js102@zepler.net>
2014-05-12 19:34:10 +0100

    make OpenFOAM mesh without mountain

---
+++ vSlice/restingScharKlemp/BTF_H/runAll/mesh.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+
+# mesh generation
+# create initial block mesh based on data in constant/polyMesh/blockMeshDict
+blockMesh
+# add a mountain range based on data in constant/add2dMountainDict
+#add2dMountain
+# plot the mesh based on parameters in constant/gmtDicts/mesh
+plotPatchData mesh
+gv constant/mesh.eps &
+
